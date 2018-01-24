002100a5d08f7d5f7251921c310e12bd5d9f0da5
Abhijit Dasgupta <dasgupab@mail.nih.gov>
2018-01-24 13:31:08 -0500

    Created tree model based on O/E>2, resolves #8

---
+++ Obs_Exp_Tree.R
@@ -0,0 +1,17 @@
+## Decision tree based on O/E > 2 at hospital level
+## Solves issue #8
+
+ProjTemplate::reload()
+
+load('data/lupuseffect.rda')
+tree_data <- hosp_data %>% left_join(oe_overall) %>% mutate(high_oe = ifelse(oe_ratio > 2, 1,0)) %>% 
+  select(new_highvolume:region, high_oe) %>% 
+  mutate(region = fct_relabel(region, str_to_title)) %>% 
+  set_names(c('High Volume','Bed Size','Teaching', 'Region', 'High O/E'))
+
+tree_model <- rpart(`High O/E` ~ ., data=tree_data)
+# tree_model_cat <- rpart(as.factor(`High O/E`)~., data=tree_data)
+
+pdf('graphs/ObsExpTree.pdf')
+prp(tree_model, type=2, extra='auto', yesno=2, fallen.leaves=F, branch=0.1, uniform=F, varlen=0, faclen=0)
+dev.off()
