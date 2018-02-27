7f248c51b5ab889afe9c4246f5c761f80c146a02
Abhijit Dasgupta <abhijit@Abhijits-MacBook-Air.local>
2018-02-26 23:55:13 -0500

    Re-orienting DataMunging
    Munge the full data first before separating
    into lupus and non-lupus data

--- DataMunging.R
+++ DataMunging.R
@@ -20,54 +20,63 @@ nonlupus_data <- dat %>% filter(lupus == 0)
 
 # Data munging ------------------------------------------------------------
 
-lupus_data <- lupus_data %>% 
-  mutate_at(vars(male, agecat,  zipinc_qrtl, #slecomb_cat, 
-                 ventilator,  starts_with('failure'), year, hospid),
-            as.factor) %>% 
-  mutate(year_scaled = scale(as.numeric(as.character(year)),
-                             center = T, scale = F)) %>% 
+dat <- dat %>% 
+  mutate_at(vars(male, agecat, zipinc_qrtl, ventilator,
+                 starts_with('failure'),
+                 year, hospid), as.factor) %>% 
+  mutate(year_scaled = scale(as.numeric(as.character(year)), center=T, scale=F)) %>% 
   mutate(payer = case_when(medicare == 1 ~ 1,
                            medicaid == 1 ~ 2,
                            private == 1 ~ 3,
                            otherins == 1 ~ 4))
+
+lupus_data <- dat %>% filter(lupus == 1)
+nonlupus_data <- dat %>% filter(lupus == 0)
+
 
 lupus_data %>% dplyr::count(hospid) %>% filter(n >= 10) %>% 
   select(hospid) %>% 
   left_join(lupus_data) -> lupus_data_10
 
-nonlupus_data <- nonlupus_data %>% 
-  mutate_at(vars(male, agecat, #payer, 
-                 zipinc_qrtl, ventilator,  starts_with('failure'), 
-                 year,
-                 hospid),
-            as.factor) %>% 
-  mutate(year_scaled = scale(as.numeric(as.character(year)),
-                             center = T, scale = F)) %>% 
-  mutate(payer = case_when(medicare == 1 ~ 1,
-                           medicaid == 1 ~ 2,
-                           private == 1 ~ 3,
-                           otherins == 1 ~ 4))
-  
 # TODO: Update this next code chunk to include the new failure variables.
+# 
+hosp_data <- dat %>% 
+  group_by(hospid) %>% 
+  summarise_at(vars(teach, highvolume, bedsize, hosp_region),
+               funs(max(.))) 
+
 hosp_data <- nonlupus_data %>% 
   group_by(hospid) %>% 
-  dplyr::summarise(nonlupus_admissions = n(), 
-            nonlupus_mortality = mean(dead),
-            nonlupus_dead = sum(dead),
-            teach = max(teach),
+  summarise_at(vars(ventilator, starts_with('failure')),
+               funs(mean(. == '1'))) %>% 
+  right_join(hosp_data)
+
+
+= max(teach),
             highvolume = max(highvolume),
-            bedsize = max(bedsize),
-            hosp_region = max(hosp_region),
-            nonlupus_vent = mean(ventilator == '1')) %>% 
+            bedsi
+hosp_data <- nonlupus_data %>% 
+  group_by(hospid) %>% 
+  dplyr::summarise(nonlupus_sepsis = n(), 
+                   nonlupus_mortality = mean(dead),
+                   nonlupus_dead = sum(dead),
+                   teach = max(teach),
+                   highvolume = max(highvolume),
+                   bedsize = max(bedsize),
+                   hosp_region = max(hosp_region)) %>% 
+  left_join(nonlupus_data %>% group_by(hospid) %>% 
+              summarise_at(vars(ventilator, starts_with('failure')),
+                           funs(mean(. == '1')))) %>% 
   inner_join(lupus_data %>% group_by(hospid) %>% 
-              summarise(lupus_admissions = n(), 
-                        lupus_mortality = mean(dead), 
-                        lupus_dead = sum(dead),
-                        lupus_vent = mean(ventilator == '1')),
-            by = 'hospid') %>% 
-  mutate(total_admissions = lupus_admissions + nonlupus_admissions,
-         prop_lupus = lupus_admissions / total_admissions)# %>% 
-  # select(-lupus_admissions, -nonlupus_admissions)
+               summarise(lupus_sepsis = n(), 
+                         lupus_mortality = mean(dead), 
+                         lupus_dead = sum(dead),
+                         lupus_vent = mean(ventilator == '1')),
+             by = 'hospid') %>% 
+  mutate(total_admissions = lupus_sepsis + nonlupus_sepsis,
+         prop_lupus = lupus_sepsis / total_admissions,
+         n_sepsis = nonlupus_sepsis + lupus_sepsis)# %>% 
+# select(-lupus_admissions, -nonlupus_admissions)
 
 common_names <- intersect(names(lupus_data), names(nonlupus_data))
 common_hosp <- intersect(lupus_data$hospid, nonlupus_data$hospid)
@@ -78,3 +87,4 @@ all_data <- rbind(lupus_data %>% filter(hospid %in% common_hosp) %>% select(comm
 save(lupus_data, lupus_data_10, nonlupus_data, hosp_data, all_data,
      file = file.path(datadir,'data','rda','exp_sepsis2','data.rda'), 
      compress = T)
+
