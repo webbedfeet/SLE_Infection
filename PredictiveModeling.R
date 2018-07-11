87b1b5b4a8c80b255ebf909bdc60f871ef9ec25d
Abhijit Dasgupta <aikidasgupta@gmail.com>
2018-07-11 10:18:06 -0400

    Looking at relationships between hospital characteristics

--- PredictiveModeling.R
+++ PredictiveModeling.R
@@ -37,6 +37,8 @@ dat <- haven::read_sas(file.path(datadir,'data','raw','exp_sepsis2.sas7bdat')) %
 file.copy('indiv_dat1_risk.csv', file.path(datadir, 'data','indiv_dat1_risk.csv'))
 file.copy('indiv_dat2_risk.csv', file.path(datadir, 'data','indiv_dat2_risk.csv'))
 
+indiv1_risk <- read_csv(file.path(datadir,'data','indiv_dat1_risk.csv'))
+
 ## Calibration of the model
 indiv1_risk %>% 
   mutate(risk_cat = cut(risk, seq(0,1, by = 0.1), include.lowest = T, right = T)) %>%
@@ -53,8 +55,7 @@ library(ROCR)
 auc <- performance(prediction(indiv1_risk$risk, as.factor(indiv1_risk$dead)),'auc')
 glue::glue('{auc@y.name}: {round(auc@y.values[[1]],3)}')
 
-
-indiv1_risk <- read_csv(file.path(datadir,'data','indiv_dat1_risk.csv'))
+## Looking at RR and patterns with hospital properties
 bl <- indiv1_risk %>% group_by(hospid, lupus) %>% 
   summarize(obs = sum(dead), expect = sum(risk)) %>% 
   ungroup() %>% 
@@ -81,12 +82,27 @@ ggplot(bl, aes(x = mr, y = RR))+geom_point(aes(size = avgN)) +
   geom_hline(yintercept = c(0.5, 1,2), linetype = c(2,1,2), color = 'red') +
   geom_vline(xintercept = c(0.1, 0.166), linetype = 2) + # quartiles
   labs(x = 'Non-lupus mortality rate')
-
-## Histogram of RR = 0 hospitals and size
-
 annual_lupus <- dat %>% mutate(hospid = as.integer(hospid)) %>% 
   group_by(hospid, year) %>% summarise(lup = sum(lupus)) %>% ungroup() %>% 
   group_by(hospid) %>% summarize(avglup = mean(lup)) %>% ungroup()
+
+## Histogram of RR = 0 hospitals and size
+bl %>% left_join(annual_lupus) %>% filter(RR == 0) %>% 
+  gather(variable, value, mr:avglup) %>% 
+  ggplot(aes(value))+geom_histogram(bins = 15) + 
+  facet_wrap(~ variable, ncol = 1, scales='free')
+bl %>% mutate(nomort = factor(ifelse(RR==0, 1, 0))) %>% 
+  ggplot(aes(x = nomort, y = mr))+geom_boxplot()
+bl %>% mutate(nomort = factor(ifelse(RR == 0, 1, 0))) %>% 
+  wilcox.test(avgN ~ nomort, data = .)
+bl %>% mutate(nomort = factor(ifelse(RR == 0, 1, 0))) %>% 
+  wilcox.test(mr ~ nomort, data = .)
+bl %>% left_join(annual_lupus) %>% mutate(nomort = ifelse(RR == 0, 1, 0)) %>% 
+  wilcox.test(avglup ~ nomort, data = .)
+bl %>% left_join(annual_lupus) %>% mutate(nomort = factor(ifelse(RR == 0 ,1 , 0))) %>% 
+  ggplot(aes(x = nomort, y = avglup))+geom_boxplot()
+## Patterns between different hospital-level metrics
+
 bl %>% left_join(annual_lupus) %>% 
   mutate(lupratio = avglup/avgN)%>% filter(RR==0) %>% 
   ggplot(aes(x = avgN, y = lupratio))+geom_point()
